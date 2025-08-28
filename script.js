4a1339fa3f18a335d92ba4ff84aa03a3de4a186d
kailanykim <145134792+kailanykim@users.noreply.github.com>
2025-08-28 11:36:13 -0300

    ..

--- script.js
+++ script.js
@@ -3,3 +3,53 @@ const caixaPergunta = document.querySelector(".caixa-pergunta");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [ 
+{
+enunciado: 
+" IAs podem tomar decisões muito mais rápidas que os humanos?",
+"verdadeiro",
+"falso"
+
+    }
+]
+
+[
+{
+enunciado: 
+" IA é sempre 100% segura e impossível de hackear?",
+"verdadeiro",
+"falso"
+
+    }
+]
+
+[
+{
+enunciado: 
+" Jogos de videogame usam IA para tornar inimigos mais inteligentes?",
+"verdadeiro",
+"falso"
+
+    }
+]
+
+    [
+{
+enunciado: 
+" IA pode clonar vozes humanas com alta precisão?",
+"verdadeiro",
+"falso"
+
+    }
+]
+
+[
+    {
+enunciado: 
+" IA vai roubar todos os empregos humanos no futuro?",
+"verdadeiro",
+"falso"
+
+    }
+]
