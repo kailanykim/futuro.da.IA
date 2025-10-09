413d1598c022597ab932164759dc5eac06e4a3d9
kailanykim <145134792+kailanykim@users.noreply.github.com>
2025-10-09 10:59:55 -0300

    .

--- script.js
+++ script.js
@@ -1,58 +1,46 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [ 
-{
-enunciado: 
-" IAs podem tomar decisões muito mais rápidas que os humanos?",
-"verdadeiro",
-"falso"
-
-    }
-]
-
-[
-{
-enunciado: 
-" IA é sempre 100% segura e impossível de hackear?",
-"verdadeiro",
-"falso"
-
-    }
-]
-
-[
-{
-enunciado: 
-" Jogos de videogame usam IA para tornar inimigos mais inteligentes?",
-"verdadeiro",
-"falso"
-
-    }
-]
-
-    [
-{
-enunciado: 
-" IA pode clonar vozes humanas com alta precisão?",
-"verdadeiro",
-"falso"
-
-    }
-]
-
-[
-    {
-enunciado: 
-" IA vai roubar todos os empregos humanos no futuro?",
-"verdadeiro",
-"falso"
-
-    }
-]
+const perguntas = [
+  {
+    enunciado: "IA vai roubar todos os empregos humanos no futuro?",
+    alternativas: [
+      { texto: "( ) Verdadeiro", afirmacao: "Verdadeiro" },
+      { texto: "( ) Falso", afirmacao: "Falso" }
+    ]
+  },
+  {
+    enunciado: "IAs podem tomar decisões muito mais rápidas que os humanos?",
+    alternativas: [
+      { texto: "( ) Verdadeiro", afirmacao: "Verdadeiro" },
+      { texto: "( ) Falso", afirmacao: "Falso" }
+    ]
+  },
+  {
+    enunciado: "A IA é sempre 100% segura e impossível de hackear?",
+    alternativas: [
+      { texto: "( ) Verdadeiro", afirmacao: "Verdadeiro" },
+      { texto: "( ) Falso", afirmacao: "Falso" }
+    ]
+  },
+  {
+    enunciado: "Jogos de videogame usam IA para tornar inimigos mais inteligentes?",
+    alternativas: [
+      { texto: "( ) Verdadeiro", afirmacao: "Verdadeiro" },
+      { texto: "( ) Falso", afirmacao: "Falso" }
+    ]
+  },
+  {
+    enunciado: "IA pode clonar vozes humanas com alta precisão?",
+    alternativas: [
+      { texto: "( ) Verdadeiro", afirmacao: "Verdadeiro" },
+      { texto: "( ) Falso", afirmacao: "Falso" }
+    ]
+  }
+];
 
 let atual = 0;
 let perguntaAtual;
